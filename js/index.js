1efd419419cac54735212fc4654aeb7996391c2e
David Warne <davidwarne@Davids-Mac.local>
2021-05-08 22:02:15 -0700

    adding more funcitons to populate the job tiles

--- js/index.js
+++ js/index.js
@@ -3,135 +3,129 @@
 "use strict";
 
 function createPMCard(obj) {
-    d3.csv("../data/job_skills.csv").then(function(data) {
-        let outerDiv = document.createElement('div');
-        outerDiv.classList.add('listing', 'container');
-        let iElement = document.createElement('i');
-        iElement.classList.add('fa', 'fa-users', 'fa-2x');
-        iElement.setAttribute('aria-hidden', 'true');
-        let innerDiv = document.createElement('div');
-        innerDiv.classList.add('description');
-        let h3 = document.createElement('h3');
-        h3.classList.add('job-name');
-        h3.textContent = obj.Title;
-        let p = document.createElement('p');
-        p.textContent = obj.Responsibilities;
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('listing', 'container');
+    let iElement = document.createElement('i');
+    iElement.classList.add('fa', 'fa-users', 'fa-2x');
+    iElement.setAttribute('aria-hidden', 'true');
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add('description');
+    let h3 = document.createElement('h3');
+    h3.classList.add('job-name');
+    h3.textContent = obj.Title;
+    let p = document.createElement('p');
+    p.textContent = obj.Responsibilities;
 
-        innerDiv.appendChild(h3);
-        innerDiv.appendChild(p);
-        outerDiv.appendChild(iElement);
-        outerDiv.appendChild(innerDiv);
-    })
+    innerDiv.appendChild(h3);
+    innerDiv.appendChild(p);
+    outerDiv.appendChild(iElement);
+    outerDiv.appendChild(innerDiv);
 }
 
 function createFinanceCard(obj) {
-    d3.csv("../data/job_skills.csv").then(function(data) {
-        let outerDiv = document.createElement('div');
-        outerDiv.classList.add('listing', 'container');
-        let iElement = document.createElement('i');
-        iElement.classList.add('fa', 'fa-usd', 'fa-2x');
-        iElement.setAttribute('aria-hidden', 'true');
-        let innerDiv = document.createElement('div');
-        innerDiv.classList.add('description');
-        let h3 = document.createElement('h3');
-        h3.classList.add('job-name');
-        h3.textContent = obj.Title;
-        let p = document.createElement('p');
-        p.textContent = obj.Responsibilities;
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('listing', 'container');
+    let iElement = document.createElement('i');
+    iElement.classList.add('fa', 'fa-usd', 'fa-2x');
+    iElement.setAttribute('aria-hidden', 'true');
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add('description');
+    let h3 = document.createElement('h3');
+    h3.classList.add('job-name');
+    h3.textContent = obj.Title;
+    let p = document.createElement('p');
+    p.textContent = obj.Responsibilities;
 
-        innerDiv.appendChild(h3);
-        innerDiv.appendChild(p);
-        outerDiv.appendChild(iElement);
-        outerDiv.appendChild(innerDiv);
-    })
+    innerDiv.appendChild(h3);
+    innerDiv.appendChild(p);
+    outerDiv.appendChild(iElement);
+    outerDiv.appendChild(innerDiv);
 }
 
 function createSalesCard(obj) {
-    d3.csv("../data/job_skills.csv").then(function(data) {
-        let outerDiv = document.createElement('div');
-        outerDiv.classList.add('listing', 'container');
-        let iElement = document.createElement('i');
-        iElement.classList.add('fa', 'fa-line-chart', 'fa-2x');
-        iElement.setAttribute('aria-hidden', 'true');
-        let innerDiv = document.createElement('div');
-        innerDiv.classList.add('description');
-        let h3 = document.createElement('h3');
-        h3.classList.add('job-name');
-        h3.textContent = obj.Title;
-        let p = document.createElement('p');
-        p.textContent = obj.Responsibilities;
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('listing', 'container');
+    let iElement = document.createElement('i');
+    iElement.classList.add('fa', 'fa-line-chart', 'fa-2x');
+    iElement.setAttribute('aria-hidden', 'true');
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add('description');
+    let h3 = document.createElement('h3');
+    h3.classList.add('job-name');
+    h3.textContent = obj.Title;
+    let p = document.createElement('p');
+    p.textContent = obj.Responsibilities;
 
-        innerDiv.appendChild(h3);
-        innerDiv.appendChild(p);
-        outerDiv.appendChild(iElement);
-        outerDiv.appendChild(innerDiv);
-    })
+    innerDiv.appendChild(h3);
+    innerDiv.appendChild(p);
+    outerDiv.appendChild(iElement);
+    outerDiv.appendChild(innerDiv);
 }
 
 function createMarketingCard(obj) {
-    d3.csv("../data/job_skills.csv").then(function(data) {
-        let outerDiv = document.createElement('div');
-        outerDiv.classList.add('listing', 'container');
-        let iElement = document.createElement('i');
-        iElement.classList.add('fa', 'fa-area-chart', 'fa-2x');
-        iElement.setAttribute('aria-hidden', 'true');
-        let innerDiv = document.createElement('div');
-        innerDiv.classList.add('description');
-        let h3 = document.createElement('h3');
-        h3.classList.add('job-name');
-        h3.textContent = obj.Title;
-        let p = document.createElement('p');
-        p.textContent = obj.Responsibilities;
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('listing', 'container');
+    let iElement = document.createElement('i');
+    iElement.classList.add('fa', 'fa-area-chart', 'fa-2x');
+    iElement.setAttribute('aria-hidden', 'true');
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add('description');
+    let h3 = document.createElement('h3');
+    h3.classList.add('job-name');
+    h3.textContent = obj.Title;
+    let p = document.createElement('p');
+    p.textContent = obj.Responsibilities;
 
-        innerDiv.appendChild(h3);
-        innerDiv.appendChild(p);
-        outerDiv.appendChild(iElement);
-        outerDiv.appendChild(innerDiv);
-    })
+    innerDiv.appendChild(h3);
+    innerDiv.appendChild(p);
+    outerDiv.appendChild(iElement);
+    outerDiv.appendChild(innerDiv);
 }
 
 function createTSCard(obj) {
-    d3.csv("../data/job_skills.csv").then(function(data) {
-        let outerDiv = document.createElement('div');
-        outerDiv.classList.add('listing', 'container');
-        let iElement = document.createElement('i');
-        iElement.classList.add('fa', 'fa-laptop', 'fa-2x');
-        iElement.setAttribute('aria-hidden', 'true');
-        let innerDiv = document.createElement('div');
-        innerDiv.classList.add('description');
-        let h3 = document.createElement('h3');
-        h3.classList.add('job-name');
-        h3.textContent = obj.Title;
-        let p = document.createElement('p');
-        p.textContent = obj.Responsibilities;
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('listing', 'container');
+    let iElement = document.createElement('i');
+    iElement.classList.add('fa', 'fa-laptop', 'fa-2x');
+    iElement.setAttribute('aria-hidden', 'true');
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add('description');
+    let h3 = document.createElement('h3');
+    h3.classList.add('job-name');
+    h3.textContent = obj.Title;
+    let p = document.createElement('p');
+    p.textContent = obj.Responsibilities;
 
-        innerDiv.appendChild(h3);
-        innerDiv.appendChild(p);
-        outerDiv.appendChild(iElement);
-        outerDiv.appendChild(innerDiv);
-    })
+    innerDiv.appendChild(h3);
+    innerDiv.appendChild(p);
+    outerDiv.appendChild(iElement);
+    outerDiv.appendChild(innerDiv);
 }
 
 function createUXCard(obj) {
-    d3.csv("../data/job_skills.csv").then(function(data) {
-        let outerDiv = document.createElement('div');
-        outerDiv.classList.add('listing', 'container');
-        let iElement = document.createElement('i');
-        iElement.classList.add('fa', 'fa-smile-o', 'fa-2x');
-        iElement.setAttribute('aria-hidden', 'true');
-        let innerDiv = document.createElement('div');
-        innerDiv.classList.add('description');
-        let h3 = document.createElement('h3');
-        h3.classList.add('job-name');
-        h3.textContent = obj.Title;
-        let p = document.createElement('p');
-        p.textContent = obj.Responsibilities;
+    let outerDiv = document.createElement('div');
+    outerDiv.classList.add('listing', 'container');
+    let iElement = document.createElement('i');
+    iElement.classList.add('fa', 'fa-smile-o', 'fa-2x');
+    iElement.setAttribute('aria-hidden', 'true');
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add('description');
+    let h3 = document.createElement('h3');
+    h3.classList.add('job-name');
+    h3.textContent = obj.Title;
+    let p = document.createElement('p');
+    p.textContent = obj.Responsibilities;
 
-        innerDiv.appendChild(h3);
-        innerDiv.appendChild(p);
-        outerDiv.appendChild(iElement);
-        outerDiv.appendChild(innerDiv);
-    })
+    innerDiv.appendChild(h3);
+    innerDiv.appendChild(p);
+    outerDiv.appendChild(iElement);
+    outerDiv.appendChild(innerDiv);
+}
+
+function renderJobListings() {
+    d3.csv("../data/job_skills.csv").then(function(data) {
+
+    });
 }
 
 // <div class="listing container">
