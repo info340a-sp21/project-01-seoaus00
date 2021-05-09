d9055e2faa26d49fa5359d1b17e812fce5d28355
David Warne <davidwarne@Davids-Mac.local>
2021-05-08 20:14:28 -0700

    trying another workaroumd to load the data from the csv

--- js/index.js
+++ js/index.js
@@ -2,29 +2,24 @@
 
 "use strict";
 
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "data.txt",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
-});
+// function() {
 
-function processData(allText) {
-    var record_num = 7;
-    var allTextLines = allText.split(/\r\n|\n/);
-    var entries = allTextLines[0].split(',');
-    var lines = [];
+//     window.addEventListener("load", init);
 
-    var headings = entries.splice(0,record_num);
-    while (entries.length>0) {
-        var tarr = [];
-        for (var j=0; j<record_num; j++) {
-            tarr.push(headings[j]+":"+entries.shift());
-        }
-        lines.push(tarr);
-    }
-}
+//     function init() {
+//     //add event listeners here
+//     //call fuctions 
+//     }
 
-processData('../data/job_skills.csv');
+//     const readcsv = require('readcsv');
+//     const file = '../data/job_skills.csv';
+
+//     readcsv (true, file, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+
+//     console.log(data);
+
+// }
