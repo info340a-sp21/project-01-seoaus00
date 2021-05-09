3e49d663553cb5c1311a57d577e3dc851d22c530
David Warne <davidwarne@Davids-Mac.local>
2021-05-08 19:33:52 -0700

    trying to figure out the javascript implementation

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
 // File for the javascript for project 1
 
+"use strict";
+
+const fs = require('fs');
+const Papa = require('papaparse');
+
+const csvFilePath = '../data/job_skills.csv';
+
+// Function to read csv which returns a promise so you can do async / await.
+
+const readCSV = async (filePath) => {
+  const csvFile = fs.readFileSync(filePath)
+  const csvData = csvFile.toString()  
+  return new Promise(resolve => {
+    Papa.parse(csvData, {
+      header: true,
+      complete: results => {
+        console.log('Complete', results.data.length, 'records.'); 
+        resolve(results.data);
+      }
+    });
+  });
+};
+
+const test = async () => {
+  let parsedData = await readCSV(csvFilePath); 
+  console.log(parsedData);
+}
+
+test();
