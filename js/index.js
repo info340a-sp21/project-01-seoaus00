29b7ff5c01e4eb61796ff9c500e1460d67d33700
David Warne <davidwarne@Davids-Mac.local>
2021-05-08 20:53:27 -0700

    fixed the functionality of reading the csv file

--- js/index.js
+++ js/index.js
@@ -3,27 +3,6 @@
 "use strict";
 
 d3.csv("../data/job_skills.csv").then(function(data) {
-    var jobs = data;
-    console.log(jobs);
+    console.log(data);
 })
-// function() {
 
-//     window.addEventListener("load", init);
-
-//     function init() {
-//     //add event listeners here
-//     //call fuctions 
-//     }
-
-//     const readcsv = require('readcsv');
-//     const file = '../data/job_skills.csv';
-
-//     readcsv (true, file, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-
-//     console.log(data);
-
-// }
